7f8ca32961ba371d55c6356e1c1d9ee140dba377
goontor <claverie.pjr@gmail.com>
2023-11-27 23:12:07 +0800

    WNS fixes

--- Timing_Issues.srcs/sources_1/new/timing_error.vhd
+++ Timing_Issues.srcs/sources_1/new/timing_error.vhd
@@ -21,7 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+use ieee.numeric_std.all;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -32,12 +32,27 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity timing_error is
---  Port ( );
+    Port (
+        i_Clk: in std_logic;
+        i_Data: in Unsigned(7 downto 0);
+        i_Data_1: in Unsigned(7 downto 0);
+        o_Data: out Unsigned(63 downto 0)
+
+    );
 end timing_error;
 
 architecture Behavioral of timing_error is
-
+    signal r0_Data  : Unsigned(7 downto 0);
+    signal r1_Data  : Unsigned(7 downto 0);
+    signal r2_Data  : Unsigned(63 downto 0);
 begin
-
-
+    process(i_Clk) is
+    begin
+        if rising_edge(i_Clk) then
+            r0_Data <= i_Data;
+            r1_Data <= i_Data_1;
+            r2_Data <= r1_Data*9*7*4;
+            o_Data <= r2_data - (((((r0_Data/3)+1)*5)/((4+5)*95))*(8*r0_Data/5+4));
+        end if;
+    end process;
 end Behavioral;
